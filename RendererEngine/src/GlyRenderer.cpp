03270ae96f0c03ac4dce78efed4ef6d021803d88
Yuri Cruz <yuriaicruz@gmail.com>
2024-03-13 14:50:12 -0300

    import and init glew library

--- RendererEngine/src/GlyRenderer.cpp
+++ RendererEngine/src/GlyRenderer.cpp
@@ -1,5 +1,8 @@
 ﻿#include "GlyRenderer.h"
+
+#include <sstream>
 #include <stdexcept>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 namespace Gly
@@ -10,13 +13,22 @@ namespace Gly
         // for (int i = 0; i < argc; ++i) {
         //     char* arg = argv[i];
         // }
-        
+
         if (!glfwInit())
         {
             throw std::runtime_error("Failed to Initialize GL");
         }
 
         window = GlyWindow(640, 480, "Engine");
+
+        auto err = glewInit();
+        if (GLEW_OK != err)
+        {
+            std::stringstream ss;
+            ss << "glewInit failed Error: ";
+            ss << glewGetErrorString(err);
+            throw std::runtime_error(ss.str());
+        }
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window.getGLFWindow()))
