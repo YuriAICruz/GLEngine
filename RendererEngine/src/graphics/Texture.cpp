8214830e3514d83679464c0f96848c6323d1d243
Yuri Cruz <yuriaicruz@gmail.com>
2024-03-20 20:36:18 -0300

    texture class

---
+++ RendererEngine/src/graphics/Texture.cpp
@@ -0,0 +1,46 @@
+﻿#include "Texture.h"
+
+#include <fstream>
+
+#include "GLTools.h"
+#include "stb_image/stb_image.h"
+
+namespace Gly
+{
+    Texture::Texture(const char* filePath)
+    {
+        stbi_set_flip_vertically_on_load(1);
+
+        localBuffer = stbi_load(filePath, &width, &height, &channels, 4);
+
+        GLCall(glGenTextures(1, &rendererId));
+        GLCall(glBindTexture(GL_TEXTURE_2D, rendererId));
+
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+        GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP));
+        GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer));
+        GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+
+        if(localBuffer)
+        {
+            stbi_image_free(localBuffer);
+        }
+    }
+
+    Texture::~Texture()
+    {
+        GLCall(glDeleteTextures(1, &rendererId));
+    }
+
+    void Texture::bind(unsigned int slot) const
+    {
+        GLCall(glActiveTexture(GL_TEXTURE0 + slot));
+        GLCall(glBindTexture(GL_TEXTURE_2D, rendererId));
+    }
+
+    void Texture::unbind() const
+    {
+        GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+    }
+}
---
+++ RendererEngine/src/graphics/Texture.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../framework.h"
+
+namespace Gly
+{
+    class GLY_API Texture
+    {
+    private:
+        unsigned int rendererId;
+        unsigned char* localBuffer;
+        int width;
+        int height;
+        int channels;
+    public:
+        Texture(const char* filePath);
+        ~Texture();
+
+        void bind(unsigned int slot = 0) const;
+        void unbind() const;
+    };
+}
